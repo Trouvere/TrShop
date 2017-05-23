f6b0a9e8368e3fabe96c7f25f546428bdf1fd1cc
Aliaksei Mishyn <spat89@mail.ru>
2017-05-23 22:11:21 +0300

    Добавление защиты по материалу http://pvtjoke.blogspot.com.by/2015/09/spring-security.html

---
+++ src/main/java/com/trouvere/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.trouvere.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.trouvere.security.TokenAuthenticationEntryPoint;
+import com.trouvere.security.TokenAuthenticationFilter;
+import com.trouvere.security.TokenAuthenticationProvider;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
+
+	@Value("${application.tokenAuthentication.header}")
+	private String header;
+
+	@Value("${application.tokenAuthentication.ignoreFault}")
+	private boolean ignoreFault;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
+				.password("adminPassword").roles("USER", "ADMIN").and().and()
+				.authenticationProvider(tokenAuthenticationProvider(auth.getDefaultUserDetailsService()));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new TokenAuthenticationFilter(authenticationManager(), tokenAuthenticationEntryPoint(),
+				header, ignoreFault), BasicAuthenticationFilter.class).sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().anyRequest()
+				.authenticated().and().httpBasic().disable().formLogin().disable().csrf().disable();
+	}
+
+	@Bean
+	public AuthenticationEntryPoint tokenAuthenticationEntryPoint() {
+		return new TokenAuthenticationEntryPoint();
+	}
+
+	@Bean
+	public AuthenticationProvider tokenAuthenticationProvider(UserDetailsService userDetailsService) {
+		return new TokenAuthenticationProvider(userDetailsService);
+	}
+}
---
+++ src/main/java/com/trouvere/security/TokenAuthenticationHeaderNotFound.java
@@ -0,0 +1,11 @@
+package com.trouvere.security;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class TokenAuthenticationHeaderNotFound extends AuthenticationException {
+
+    public TokenAuthenticationHeaderNotFound(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+}
